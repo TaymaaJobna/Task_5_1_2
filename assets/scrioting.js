71746c82a2611d18ee4bf30169e386f5a278c24f
TaymaaJobna <taymaajobnah98@gmail.com>
2023-10-07 12:28:56 -0400

    Slider Task

---
+++ assets/scrioting.js
@@ -0,0 +1,51 @@
+//access the images
+let slideImages = document.querySelectorAll('img');
+
+//access next prev buttons
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+
+//access the indicators
+let dots = document.querySelectorAll('.dot');
+
+var counter = 0;
+
+//code for next button
+next.addEventListener('click', function slidenext() {
+    slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+    if (counter >= slideImages.length - 1) {
+        counter = 0;
+    } else {
+        counter++;
+    }
+    slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+})
+
+//code for prev button
+prev.addEventListener('click', function slidprev() {
+    slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+    if (counter == 0) {
+        counter = slideImages.length - 1;
+    } else {
+        counter--;
+    }
+    slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+})
+
+//add event to indicator
+function switchImage(currentImage) {
+    currentImage.classList.add('active');
+    var imageId = currentImage.getAttribute('attr');
+    if (imageId > counter) {
+        slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
+        counter = imageId;
+        slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
+    }
+    else if (imageId == counter) {
+        return;
+    } else {
+        slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
+        counter = imageId;
+        slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
+    }
+}
